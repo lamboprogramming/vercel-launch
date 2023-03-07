fb1a009802c396f03e8be54e85221e4a2dd20451
Lamboprogramming <gillespielamar@gmail.com>
2023-03-07 02:49:39 -0500

    bg img not working

---
+++ djluchainz/components/Heropage.jsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import bg from '../public/images/nighttime.jpg'
+import twin from '../public/images/twindj.png'
+import Link from 'next/link'
+import Social from './Social'
+
+
+const Heropage = () => {
+    const backgroundImageStyle = {
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: "cover",
+      }
+
+    const bgImage = {
+        src: '/../public/images/nighttime.jpg',
+        alt: 'Background image'
+    };
+    
+  return (
+    
+    <div className="relative grid h-screen grid-cols-1 hero"> 
+    <Image
+        src={bgImage.src}
+        alt={bgImage.alt}
+        fill
+        cover
+        quality={100}
+    />
+    <div className="flex-col w-full h-screen pb-20 lg:block hero-content lg:flex-row-reverse md:mb-20">
+            <Image src={twin} className="max-w-sm md:max-w-md lg:mt-20 mb:20 " alt="#"/>
+        <div className="mt-4 text-center">
+
+      {/* Contact Button */}
+            <Link href="/contact">
+                <button className="px-14 py-[10%]  text-2xl  text-white   border-2 border-white rounded-md bg-gradient-to-r from-transparent to-indigo-900 hover:from-indigo-900 hover:to-fuchsia-900 sm:w-[20] mr-1  nav-links tracking-widest mt-2  lg:mt-4 font-sans  w-full ">Request a Quote
+                </button>
+            </Link> 
+        </div>     
+    </div>
+    </div>
+      
+      
+  );
+}
+export default Heropage;
--- djluchainz/src/pages/index.js
+++ djluchainz/src/pages/index.js
@@ -3,11 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Footer from '../../components/Footer'
 import Link from 'next/link'
-import { useState } from 'react'
 import Navbar from '../../components/Navbar'
-import Hero from '../../components/Hero'
-import bg from '../../public/images/nighttime.jpg'
-import twin from '../../public/images/twindj.png'
+import Heropage from '../../components/Heropage'
+
 
 
 
@@ -17,49 +15,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
-    const [navbar, setNavbar] = useState(false);
 
-    const backgroundImageStyle = {
-        backgroundImage: `url(${bg.src})`,
-        backgroundSize: "cover",
-      }
-
-    const bgImage = {
-        src: '/../public/images/nighttime.jpg',
-        alt: 'Background image'
-    };
   return (
     
     <>
       <Navbar />
-
-      {/* Hero */}
-      
-      
-      <div className="relative grid w-full h-screen grid-cols-1 hero"> 
-      <Image
-          src={bgImage.src}
-          alt={bgImage.alt}
-          fill
-          cover
-          quality={100}
-      />
-      <div className="flex-col w-full h-screen pb-20 lg:block hero-content lg:flex-row-reverse md:mb-20">
-              <Image src={twin} className="max-w-sm md:max-w-md lg:mt-20 mb:20 " alt="#"/>
-          <div className="mt-4 text-center">
-
-        {/* Contact Button */}
-              <Link href="/contact">
-                  <button className="px-14 py-[10%]  text-2xl  text-white   border-2 border-white rounded-md bg-gradient-to-r from-transparent to-indigo-900 hover:from-indigo-900 hover:to-fuchsia-900 sm:w-[20] mr-1  nav-links tracking-widest mt-2  lg:mt-4 font-sans  w-full ">Request a Quote
-                  </button>
-              </Link> 
-          </div>     
+      <div className="bg-gray-900 ">
+        <Heropage />
       </div>
-  </div>
-        
-    
-      {/* Hero End */}
-      
       <Footer />
     </>
   )
