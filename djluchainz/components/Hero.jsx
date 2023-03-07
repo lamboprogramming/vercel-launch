0fa96cf0789cd01e304c613f352aff2f562565ee
Lamboprogramming <gillespielamar@gmail.com>
2023-03-06 23:41:14 -0500

    fixed position on home page

--- djluchainz/components/Hero.jsx
+++ djluchainz/components/Hero.jsx
@@ -10,16 +10,16 @@ const Heropage = () => {
         backgroundSize: "cover",
       }
   return (
-        <div className="grid w-full h-screen grid-cols-1 hero" style={backgroundImageStyle}> 
+        <div className="fixed grid w-full h-screen grid-cols-1 hero " style={backgroundImageStyle}> 
             <div className="flex-col lg:block hero-content lg:flex-row-reverse">
-                <Image src={twin} className="max-w-sm shadow-3xl rounded-xl lg:hidden" alt="#"/>
+                <Image src={twin} className="max-w-lg p-4 shadow-3xl rounded-xl lg:hidden" alt="#"/>
                 <div className="mt-4 text-center">
  
-                    <h1 className="text-5xl font-bold font-[Abel] tracking-wider text-yellow-400 md:text-7xl text-center mt-4 mb-4 shadow-2xl hidden lg:block ">DJ LUCHAINZ</h1>
+                    <h1 className="hidden mt-4 mb-4 font-sans text-5xl font-bold tracking-wider text-center shadow-2xl md:text-7xl lg:block ">DJ LUCHAINZ</h1>
 
               {/* Contact Button */}
               <Link href="/contact">
-              <button className="px-[8rem] py-[3%]  text-2xl  text-white lg:text-yellow-400  border-2 border-white lg:border-yellow-400 rounded-md bg-gradient-to-r from-transparent to-indigo-900 hover:from-indigo-900 hover:to-fuchsia-900 sm:w-[20] mr-1  nav-links tracking-widest mt-2 md:mt-4 lg:mt-10 font-sans ">Request a Quote
+              <button className="px-[8rem] py-[3%]  text-2xl  text-white   border-2 border-white rounded-md bg-gradient-to-r from-transparent to-indigo-900 hover:from-indigo-900 hover:to-fuchsia-900 sm:w-[20] mr-1  nav-links tracking-widest mt-2 md:mt-4 lg:mt-10 font-sans ">Request a Quote
               </button>
          </Link> 
                 </div>  
