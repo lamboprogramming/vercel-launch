6cb126d28842d3a1924400c2545b18cac83d96c1
Lamboprogramming <gillespielamar@gmail.com>
2023-04-07 15:02:27 -0400

    same changes applied

--- djluchainz/components/ContactForm.jsx
+++ djluchainz/components/ContactForm.jsx
@@ -21,7 +21,7 @@ const ContactForm = () => {
   }
 
   return (
-      <div className="grid grid-cols-1 font-sans bg-slate-100">
+      <div className="grid grid-cols-1 py-4 font-sans bg-slate-100">
   {/* Col 2 contact form */}
 
   <div className="container mx-auto ">
@@ -33,33 +33,33 @@ const ContactForm = () => {
       <h1 className='font-sans font-[500] text-3xl text-black text-left pb-2 sm:pb-4 pt-2 tracking-wide'>Contact Us</h1>
       {/* Name */}
         <div className="mb-6 ">
-          <input type="text" className="w-full px-3 py-2 leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600 " name="name"
+          <input type="text" className="w-full px-3 py-2 text-lg leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600 " name="name"
             placeholder="Name"
             />
         </div>
       {/* Email */}
         <div className="mb-6 ">
-          <input type="email" className="w-full px-3 py-2 leading-tight text-gray-700 capitalize bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" name="email"
+          <input type="email" className="w-full px-3 py-2 text-lg leading-tight text-gray-700 capitalize bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" name="email"
             placeholder="Email address"
             />
         </div>
       {/* City/State */}
         <div className="mb-6 ">
-          <input type="text" className="w-full px-3 py-2 leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" name="location"
+          <input type="text" className="w-full px-3 py-2 text-lg leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600 " name="location"
             placeholder="City/State" />
         </div>
       {/* Date */}
         <div className="mb-6 ">
-          <input type="text" placeholder='Date of Event' className="w-full px-3 py-4 leading-tight text-gray-400 bg-white border rounded shadow appearance-none md:py-2 focus:outline-none focus:bg-white focus:border-blue-600" name="date" />
+          <input type="text" placeholder='Date of Event' className="w-full px-3 py-4 text-lg leading-tight text-gray-400 bg-white border rounded shadow appearance-none md:py-2 focus:outline-none focus:bg-white focus:border-blue-600 " name="date" />
         </div>
       {/* Category */}
         <div className="mb-6 ">
-          <select type="checkbox" className="w-full px-3 py-2 leading-tight text-gray-400 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" id="category" name="category" placeholder="Event Type'">
-          <option value="">--Please choose an option--</option>
-          <option value="wedding">Wedding ($250/Hour)</option>
-          <option value="corporate">Corporate Event ($175/Hour)</option>
-          <option value="private">Private Event ($100/Hour)</option>
-          <option value="lessons">DJ Lessons ($75/Hour)</option>
+          <select type="checkbox" className="w-full px-3 py-2 text-lg leading-tight text-gray-400 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" id="category" name="category" placeholder="Event Type'">
+          <option value="">--Choose An Option--</option>
+          <option value="wedding">Wedding $250/Hour</option>
+          <option value="corporate">Corporate Event $175/Hour</option>
+          <option value="private">Private Event $100/Hour</option>
+          <option value="lessons">DJ Lessons $40/Hour</option>
           <option value="other">Other</option>
           
           </select>
@@ -69,7 +69,7 @@ const ContactForm = () => {
    
         {/* Text */}
         <div >
-          <textarea className="w-full px-3 py-2 leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600" id="area" rows="3" placeholder="Message" name="message" ></textarea>
+          <textarea className="w-full px-3 py-2 text-lg leading-tight text-gray-700 bg-white border rounded shadow appearance-none focus:outline-none focus:bg-white focus:border-blue-600 " id="area" rows="3" placeholder="Message" name="message" ></textarea>
         </div>
         <div className="flex justify-center">
         <button type="submit" className="items-center block px-16 py-4 mt-4 text-sm font-semibold leading-4 text-center text-white rounded-lg shadow-md bg-cyan-500 hover:bg-cyan-600">Submit</button>
